f0310607c16407501836d5cf16f0ff9432e3cfd4
Валерия <valeria.tarasenko.a@gmail.com>
2020-09-27 12:50:06 +0300

    parse JSON

--- Sunny/Models/NetworkWeatherManager.swift
+++ Sunny/Models/NetworkWeatherManager.swift
@@ -11,17 +11,29 @@ import Foundation
 struct NetworkWeatherManager {
     
     func fetchCurrentWeather(forCity city: String){
-    let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)"
-    
-    guard let url = URL(string: urlString) else { return }
-    //работа с url ведется сессионно
-    let session = URLSession(configuration: .default)
-    let task = session.dataTask(with: url) { data, response, error in
-        if let data = data {
-            guard let dataString = String(data: data, encoding: .utf8) else { return }
-            print(dataString)
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)"
+        
+        guard let url = URL(string: urlString) else { return }
+        //работа с url ведется сессионно
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, response, error in
+            if let data = data {
+                self.parseJSON(withjJSON: data)
+            }
         }
+        task.resume() //запуск
     }
-    task.resume() //запуск
+    
+    func parseJSON(withjJSON data: Data) {
+        let decoder = JSONDecoder()
+        
+        do {
+            let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
+            print(currentWeatherData.main.temp)
+        } catch let error as NSError {
+            print(error.localizedDescription )
+        }
+        
     }
+    
 }
